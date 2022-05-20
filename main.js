7f621631ec89df7d70a9e5f9998a0100b49e6c34
jltml <8261330+jltml@users.noreply.github.com>
2022-05-19 21:49:02 -0500

    Initial commit!

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+import Cleave from 'cleave.js';
+import 'cleave.js/dist/addons/cleave-phone.US';
+var cleave = new Cleave('.cleave-phone', {
+    phone: true,
+    phoneRegionCode: 'US',
+});
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
